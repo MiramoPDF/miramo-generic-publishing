eadb1684aa1925ae00da37f5b53211aa792ad379
MiramoPDF <80524441+MiramoPDF@users.noreply.github.com>
2025-11-17 13:31:36 +0000

    DZ: reduced warn level in miramoRP.cmd

--- bin/miramoRP.cmd
+++ bin/miramoRP.cmd
@@ -1,4 +1,3 @@
 @ECHO OFF
 set PATH=%~d0%~p0;%PATH%
-echo %PATH%
-mmClient.exe -user mmUser -password mmUser -networked Y -secure -port 443 -warn 4 %*
+mmClient.exe -user mmUser -password mmUser -networked Y -secure -port 443 -warn 2 %*
